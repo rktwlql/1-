781dab9f239c68e7225d5da5e8519498208942f1
rktwlql <50227985+rktwlql@users.noreply.github.com>
2024-04-16 10:52:43 +0900

    Add files via upload

---
+++ 제 6장/연습문제 1,2,3,4번.py
@@ -0,0 +1,25 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #문제 1번
+>>> # 입력 관련 함수 : input(), read(), read
+KeyboardInterrupt
+>>> # 입력 관련 함수 : input(), read(), readline(), readlines()
+>>> # 출력 관련 함수 : print(), write(), writeline()
+>>> 
+>>> #문제 2번
+>>> #문제 2번
+>>> 
+>>> #열고-읽고-쓰고-닫고 가 적합하다. 답은 A-C-B-D인 1번
+>>> 
+>>> #문제 3번
+>>> 
+>>> #보기 1, 읽기 모드가 기본값이다.
+>>> #보기 3, 쓰기 모드임은 맞으나 기존 파일에 이어서 쓴다.
+>>> # 답은 1번과 3번.
+>>> 
+>>> #문제 4번
+>>> 
+>>> #먼저 파일을 열어야 하기에 open을 첫 칸에 채우고
+>>> #1개의 행만 읽기 위해서 readline을 사용한다.
+>>> #그 후 파일을 닫기 위해 close를 사용한다.
+>>> 
---
+++ 제 6장/연습문제 6번.py
@@ -0,0 +1,6 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #문제 6번
+>>> 
+>>> # 파일을 복사하기 위해서는 해당 파일 전체를 읽어와야 한다. 그렇기에 readlines()를 첫 칸에 사용한다.
+>>> # 그 후 outFp의 내용을 inFp로 채우기 위해 'outFp.writelines(inStr)'을 채워 해당 내용을 복사한다.
